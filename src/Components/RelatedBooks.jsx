ec3361c7067a0fb534f4532eb50f8c0cf6f0b539
User Name <email>
2025-09-07 18:23:27 +0200

    relatedupdate6

--- src/Components/RelatedBooks.jsx
+++ src/Components/RelatedBooks.jsx
@@ -27,8 +27,7 @@ export default function BookDetail() {
   useEffect(() => {
     // Fetch the main book
     axios
-      .get(`https://backendsvkwbshp.onrender.com/api/books/
-        ${id}`)
+      .get(`https://backendsvkwbshp.onrender.com/api/books/${id}`)
       .then((res) => {
         setBook(res.data);
         // After fetching the main book, fetch related books
