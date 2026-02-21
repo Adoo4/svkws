b2d773e700ef82824f4db39a6deb0e974d12ad0a
User Name <email>
2026-02-21 16:17:37 +0100

    test produkcije

--- src/Components/CardPicker3.jsx
+++ src/Components/CardPicker3.jsx
@@ -21,7 +21,7 @@ const CardPicker = ({
       id: "cash",
       label: "Pouzećem",
       description: "Plaćanje gotovinom pri preuzimanju paketa",
-      disabled: true,
+      disabled: false,
     },
 
     {
